711eed0bcaa9d221ad95761eb1d8f45a27c85d3a
somedave <somedave@users.noreply.github.com>
2017-01-14 09:23:22 -0700

    udpated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "ENV['EMAIL_DEFAULT_URL_OPTIONS']"}
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['EMAIL_SERVER'],
     domain: ENV['EMAIL_DOMAIN'],
