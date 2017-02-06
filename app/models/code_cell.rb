7292e6330677b8f9f148737a26f7dbabeff6a454
Jeff <jeffyjefflabs@users.noreply.github.com>
2017-02-06 09:22:23 -0700

    Helper to get from code cell to code

--- app/models/code_cell.rb
+++ app/models/code_cell.rb
@@ -4,4 +4,10 @@ class CodeCell < ActiveRecord::Base
   has_many :executions, dependent: :destroy
 
   validates :md5, :ssdeep, :notebook, :cell_number, presence: true
+
+  # Return code associated with this cell
+  # Note: this is not particularly efficient
+  def source
+    notebook.notebook.code_cells_source[cell_number]
+  end
 end
