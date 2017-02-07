4a87b29151bc8a16e19f1c3d3fa09400a6e1d537
somedave <team@nb.gallery>
2017-02-06 19:54:28 -0700

    added stub

--- config/initializers/gallery.rb
+++ config/initializers/gallery.rb
@@ -40,6 +40,7 @@ stubs = [
   'app/views/application/_custom_footer.slim',
   'app/views/application/_custom_fields.slim',
   'app/views/application/_custom_buttons.slim',
+  'app/views/application/_custom_webtracking.slim',
   'app/views/static_pages/_custom_overview_modal.slim',
   'app/views/application/_custom_change_request_approval_fields.slim',
   'app/views/application/_custom_change_request_warning.slim',
