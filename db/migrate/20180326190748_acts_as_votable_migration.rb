82bf16e02ea0b2a95c0d3d26b832fc1763f1e552
Jeff <jeffyjefflabs@users.noreply.github.com>
2018-03-30 09:57:10 -0600

    Rubocop commenting migrations

--- db/migrate/20180326190748_acts_as_votable_migration.rb
+++ db/migrate/20180326190748_acts_as_votable_migration.rb
@@ -1,9 +1,9 @@
+# Add votes table for comment upvoting
 class ActsAsVotableMigration < ActiveRecord::Migration
   def self.up
     create_table :votes do |t|
-
-      t.references :votable, :polymorphic => true
-      t.references :voter, :polymorphic => true
+      t.references :votable, polymorphic: true
+      t.references :voter, polymorphic: true
 
       t.boolean :vote_flag
       t.string :vote_scope
@@ -12,8 +12,8 @@ class ActsAsVotableMigration < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :votes, [:voter_id, :voter_type, :vote_scope]
-    add_index :votes, [:votable_id, :votable_type, :vote_scope]
+    add_index :votes, %i[voter_id voter_type vote_scope]
+    add_index :votes, %i[votable_id votable_type vote_scope]
   end
 
   def self.down
