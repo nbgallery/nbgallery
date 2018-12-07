9450e149b39a033e61e0529ec11a8ad9fe2927cf
Jeff <jeffyjefflabs@users.noreply.github.com>
2018-12-06 20:04:47 -0700

    Remove old non-matrix computations

--- app/models/user_similarity.rb
+++ app/models/user_similarity.rb
@@ -10,50 +10,6 @@ class UserSimilarity < ActiveRecord::Base
   validates :user, :other_user, :score, presence: true
 
   # Cosine similarity score
-  # TODO: remove old non-matrix version
-  def self.vector_similarity(vi, vj)
-    return 0.0 if vi.empty? or vj.empty?
-    m1 = Math.sqrt(vi.map {|_id, x| x * x}.reduce(0, :+))
-    m2 = Math.sqrt(vj.map {|_id, x| x * x}.reduce(0, :+))
-    dot = vi.map {|id, x| x * vj.fetch(id, 0.0)}.reduce(:+)
-    dot / (m1 * m2)
-  end
-
-  # TODO: remove old non-matrix version
-  def self.compute(which=nil)
-    max_per_user = 50
-
-    # Get feature vectors for all users
-    vectors = {}
-    User.includes(:stars).find_each(batch_size: 100) do |user|
-      vectors[user.id] = user.feature_vector
-    end
-
-    # Compute similarity; keep top N for each user
-    vectors.each do |i, vi|
-      next if which && which != i
-      scores = []
-      vectors.each do |j, vj|
-        next if i == j
-        score = vector_similarity(vi, vj)
-        next if score < 0.2 # don't bother with low scores
-        scores.push([j, score])
-      end
-      scores = scores.sort_by {|_j, score| -score}.take(max_per_user)
-      records = scores.map do |j, score|
-        UserSimilarity.new(
-          user_id: i,
-          other_user_id: j,
-          score: score
-        )
-      end
-      UserSimilarity.transaction do
-        UserSimilarity.where(user_id: i).delete_all # no callbacks
-        UserSimilarity.import(records, validate: false)
-      end
-    end
-  end
-
   # Compute similarity for all user pairs simultaneously using sparse matrix
   def self.matrix_compute # rubocop: disable Metrics/AbcSize
     # List of unique [user id, notebook id, score] actions where score is 1.0
--- app/models/users_also_view.rb
+++ app/models/users_also_view.rb
@@ -7,45 +7,6 @@ class UsersAlsoView < ActiveRecord::Base
 
   # Nightly computation methods
   class << self
-    # Compute score to reflect user view overlap
-    def compute(which=nil)
-      max_per_notebook = 50
-
-      # Compute set of users viewing each notebook
-      clicks = {}
-      ids = []
-      Notebook.find_each(batch_size: 100) do |nb|
-        ids.push(nb.id)
-        nb_clicks = nb.clicks.where('updated_at > ?', 90.days.ago).select(:user_id).distinct.pluck(:user_id)
-        clicks[nb.id] = Set.new(nb_clicks)
-      end
-
-      # Compute overlap score using Jaccard index
-      ids.each do |i|
-        next if which && which != i
-        scores = []
-        ids.each do |j|
-          next if i == j
-          intersection = clicks[i].intersection(clicks[j]).count
-          union = clicks[i].union(clicks[j]).count
-          jaccard = union.nonzero? ? intersection.to_f / union : 0.0
-          scores.push([j, jaccard])
-        end
-        scores = scores.sort_by {|_j, jaccard| -jaccard}.take(max_per_notebook)
-        records = scores.map do |j, jaccard|
-          UsersAlsoView.new(
-            notebook_id: i,
-            other_notebook_id: j,
-            score: jaccard
-          )
-        end
-        UsersAlsoView.transaction do
-          UsersAlsoView.where(notebook_id: i).delete_all # no callbacks
-          UsersAlsoView.import(records, validate: false)
-        end
-      end
-    end
-
     def matrix_compute # rubocop: disable Metrics/AbcSize
       # List of unique [notebook id, user id] actions
       clicks = Click
