323ba4f59de6f0719bafc8445c61bede2b13a798
mcrutch <51446717+mcrutch@users.noreply.github.com>
2020-09-09 13:45:26 +0000

    Fixing form nesting and approved/unapproved logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -117,7 +117,7 @@ class User < ActiveRecord::Base
   scope :filter_by_first_name, -> (name) { where( "first_name like ?", "%#{name}%" ) }
   scope :filter_by_last_name, -> (name) { where( "last_name like ?", "%#{name}%" ) }
   scope :filter_by_org, -> (org) { where( "org like ?","%#{org}%" ) }
-  scope :filter_by_approved, -> (approved) { where( "approved = ? or (approved IS NULL and 1 = ?)", approved, approved ) }
+  scope :filter_by_approved, -> (approved) { where( "approved = ? or (approved IS NULL and 0 = ?)", approved, approved ) }
   scope :filter_by_admin, -> (admin) { where admin: admin }
 
   extend Forwardable
