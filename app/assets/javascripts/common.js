6bfc487d934063b944556bbe4449da999962a0ae
manfromjupyter <51969207+manfromjupyter@users.noreply.github.com>
2022-06-13 13:12:38 +0000

    Syntax cleanup mostly

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,18 +2,18 @@
 /* =============== Alerts ============== */
 /* ===================================== */
 /* ===== Make Inline Alert ===== */
-function makeAlert(type, element_within, message) {
-  if (element_within == '') {
+function makeAlert(type, element_within, message){
+  if (element_within == ''){
     element_within = '#main .alert-container:first';
   }
   // NOTE - this shall only be used to generate messages on pages that are not going to be reloaded. If you wish to reload the page AND have an alert when it loads, alert needs to be generated from backend.
-  if (type == "success") {
+  if (type == "success"){
     $(element_within).prepend("<div class='alert alert-success' role='alert'><i aria-hidden='true' class='fa fa-check-circle'></i>" + message + "<button aria-label='Dismiss alert' class='close' data-dismiss='alert'>&times;</button></div>");
   }
-  else if (type = "error") {
+  else if (type = "error"){
     $(element_within).prepend("<div class='alert alert-error' role='alert'><i aria-hidden='true' class='fa fa-times-circle'></i>" + message + "<button aria-label='Dismiss alert' class='close' data-dismiss='alert'>&times;</button></div>");
   }
-  else if (type == "warning") {
+  else if (type == "warning"){
     $(element_within).prepend("<div class='alert alert-warning' role='alert'><i aria-hidden='true' class='fa fa-exclamation-triangle'></i>" + message + "<button aria-label='Dismiss alert' class='close' data-dismiss='alert'>&times;</button></div>");
   }
   else {
@@ -24,7 +24,7 @@ function makeAlert(type, element_within, message) {
 }
 
 /* ===== Make Screenreader-Only Alert ===== */
-function makeScreenreaderAlert(element_id, on_message, off_message) {
+function makeScreenreaderAlert(element_id, on_message, off_message){
   var element = '#' + element_id;
   if ($(element).length){
     $(element).text(message);
@@ -37,19 +37,19 @@ function makeScreenreaderAlert(element_id, on_message, off_message) {
 /* ===================================== */
 /* ========= Clean JSON Parse ========== */
 /* ===================================== */
-function cleanJSON(json) {
+function cleanJSON(json){
   var keys = Object.keys(json);
   var string = '';
-  for (let i = 0; i < keys.length; i++) {
+  for (let i = 0; i < keys.length; i++){
     string += keys[i] + ' ' + json[keys[i]];
-    if (i + 1 != keys.length) {
-      if (keys.length >= 3 && i + 2 == keys.length) {
+    if (i + 1 != keys.length){
+      if (keys.length >= 3 && i + 2 == keys.length){
         string += ', and '
       }
-      else if (keys.length > 2) {
+      else if (keys.length > 2){
         string += ', '
       }
-      else if (keys.length == 2) {
+      else if (keys.length == 2){
         string += ' and '
       }
     }
@@ -63,9 +63,9 @@ function cleanJSON(json) {
 /* ======================================= */
 /* ===== Expand Textarea as you type ===== */
 /* ======================================= */
-function autoSize({target:element}) {
+function autoSize({target:element}){
   // Only expands if they have the "auto-expand" class and the keydown autoSize event listener
-  if(!element.classList.contains('auto-expand'))
+  if (!element.classList.contains('auto-expand'))
     return;
   setTimeout(function(){
     value = element.scrollHeight + 2;
@@ -76,15 +76,15 @@ function autoSize({target:element}) {
 /* ===================================== */
 /* ========= Character Limits ========== */
 /* ===================================== */
-function remainingCharacterWarning(length, characterCountElement, maxlength) {
+function remainingCharacterWarning(length, characterCountElement, maxlength){
   $(characterCountElement).html( 'Remaining characters: ' + ( maxlength - length ));
-  if (maxlength <= length) {
+  if (maxlength <= length){
     $(characterCountElement).addClass('error');
   }
   else {
     $(characterCountElement).removeClass('error');
   }
-  if (maxlength - length < 50) {
+  if (maxlength - length < 50){
     $(characterCountElement).css('display','block');
   }
   else {
@@ -95,7 +95,7 @@ function remainingCharacterWarning(length, characterCountElement, maxlength) {
 /* ===================================== */
 /* =========== Loading Gif ============= */
 /* ===================================== */
-function loadingGif() {
+function loadingGif(){
   $(document).ajaxStart(function(){
     $('#hiddenSpinner').addClass("loading");
     $('#hiddenSpinner').attr("aria-live","assertive");
