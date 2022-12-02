4893eaee4560916448d74eb89c4932ffc72ee113
Vincenzo <vcriscuolo@microsoft.com>
2022-12-02 12:57:25 +0000

    finished rest of rails diff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,12 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'flash')
-
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile = %w( search.js )
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
-Rails.application.config.assets.precompile += %w[
+Rails.application.config.assets.precompile = %w[
   commontator/upvote.png
   commontator/upvote_active.png
   commontator/upvote_disabled.png
