f9771bb12d59611a06f4da12d145c3d8a15842cd
mcrutch <51446717+mcrutch@users.noreply.github.com>
2022-12-07 15:14:14 +0000

    Support changes from jquery-ujs to rails-ujs

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,38 +1,49 @@
 $(document).ready(function() {
-  $("#sign_in_user").on("ajax:success", function(e, data, status, xhr) {
-    window.location.reload(true)
-  })
-  $("#sign_in_user").on("ajax:error", function(e, data, status, xhr) {
-    console.log("Failed to log in", data.responseJSON)
-    $(".login-flashes p").remove()
-    $(".login-flashes").append("<p class='text-danger'>" + data.responseJSON.error + "</p>")
-  })
-  $("#sign_up_user").on("ajax:success", function(e, data, status, xhr) {
-    console.log("Successfully registered user")
-    $(".login-flashes p").remove()
-    $(".login-flashes").append("<p class='text-success'>Check your e-mail for a link to confirm your account</p>")
-    $(".login-flashes").append("<p class='text-success'>You must confirm your account before you can log in</p>")
-  })
-  $("#sign_up_user").on("ajax:error", function(e, data, status, xhr) {
-    console.log("Failed to register user", data.responseJSON)
-    console.log(data.responseJSON)
-    $(".login-flashes p").remove()
-    if (data.responseJSON.errors.email) {
-      $(".login-flashes").append("<p class='text-danger'>E-mail already in use or invalid</p>")
-    }
-    if (data.responseJSON.errors.user_name) {
-      $(".login-flashes").append("<p class='text-danger'>User name already in use or invalid</p>")
-    }
-  })
-  $("#reset_password").on("ajax:success", function(e, data, status, xhr) {
-    console.log("Successfully reset password")
-    $(".login-flashes p").remove()
-    $(".login-flashes").append("<p class='text-success'>Check your e-mail for a link to reset your password</p>")
-  })
-  $("#reset_password").on("ajax:error", function(e, data, status, xhr) {
-    console.log("Failed to reset_password", data.responseJSON)
-    $(".login-flashes p").remove()
-    $(".login-flashes").append("<p class='text-danger'>Invalid e-mail address</p>")
-    $(".login-flashes").append("<p class='text-danger'>Please verify it is correct and try again</p>")
-  })
+  if(element=document.querySelector("#sign_in_user")){
+    element.addEventListener("ajax:success", function(event) {
+      window.location.reload(true);
+    })
+    element.addEventListener("ajax:error", function(event) {
+      var detail = event.detail;
+      var data = detail[0], status = detail[1],  xhr = detail[2];
+      console.log("Failed to log in", data);
+      $(".login-flashes p").remove();
+      $(".login-flashes").append("<p class='text-danger'>" + data.error + "</p>");
+    })
+  }
+  if(element=document.querySelector("#sign_up_user")){
+    element.addEventListener("ajax:success", function(event) {
+      console.log("Successfully registered user");
+      $(".login-flashes p").remove();
+      $(".login-flashes").append("<p class='text-success'>Check your e-mail for a link to confirm your account</p>");
+      $(".login-flashes").append("<p class='text-success'>You must confirm your account before you can log in</p>");
+    })
+    element.addEventListener("ajax:error", function(event) {
+      var detail = event.detail;
+      var data = detail[0], status = detail[1],  xhr = detail[2];
+      console.log("Failed to register user", data);
+      $(".login-flashes p").remove();
+      if (data.errors.email) {
+        $(".login-flashes").append("<p class='text-danger'>E-mail already in use or invalid</p>");
+      }
+      if (data.errors.user_name) {
+        $(".login-flashes").append("<p class='text-danger'>User name already in use or invalid</p>");
+      }
+    })
+  }
+  if(element=document.querySelector("#reset_password")){
+    element.addEventListener("ajax:success", function(event) {
+      console.log("Successfully reset password");
+      $(".login-flashes p").remove();
+      $(".login-flashes").append("<p class='text-success'>Check your e-mail for a link to reset your password</p>");
+    })
+    element.addEventListener("ajax:error", function(event) {
+      var detail = event.detail;
+      var data = detail[0], status = detail[1],  xhr = detail[2];
+      console.log("Failed to reset_password", data);
+      $(".login-flashes p").remove();
+      $(".login-flashes").append("<p class='text-danger'>Invalid e-mail address</p>");
+      $(".login-flashes").append("<p class='text-danger'>Please verify it is correct and try again</p>");
+    })
+  }
 })
