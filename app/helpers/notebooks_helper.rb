480868b686b77d3d31b8531ea6cb259bfd210e1f
mcrutch <51446717+mcrutch@users.noreply.github.com>
2023-01-25 19:47:15 +0000

    Cleanup markdown pipeline and remove unnecessary gems

--- app/helpers/notebooks_helper.rb
+++ app/helpers/notebooks_helper.rb
@@ -1,103 +1,8 @@
-require 'kramdown/converter/syntax_highlighter/rouge'
-require 'github/markup'
-
 # Helper for rendering notebooks
 module NotebooksHelper
-  # Adds support for embedding markdown in slim
-  class Renderer < Slim::Embedded::InterpolateTiltEngine
-    # Add rouge syntax highlighting to redcarpet renderer
-    class SyntaxHighlighting < Redcarpet::Render::HTML
-      def block_code(code, language)
-        language = 'text' if language.blank?
-        begin
-          Rouge.highlight code, language, 'html'
-        rescue StandardError
-          Rouge.highlight code, 'text', 'html'
-        end
-      end
-    end
-
-    # Convert bare URLs into links
-    def autolink(html)
-      pipeline = HTML::Pipeline.new([HTML::Pipeline::AutolinkFilter])
-      pipeline.call(html)[:output].to_s
-    end
-
-    # Render with redcarpet
-    # Known problems:
-    #   * ``` code blocks without language render as inline code
-    def render_markdown_redcarpet(text)
-      markdown = Redcarpet::Markdown.new(
-        SyntaxHighlighting,
-        tables: true,
-        fenced_code_blocks: true
-      )
-      autolink(markdown.render(text))
-    end
-
-    # Render with kramdown
-    # Known problems:
-    #   * Math hack - see below
-    def render_markdown_kramdown(text)
-      markdown = Kramdown::Document.new(text, input: 'GFM', syntax_highlighter: :rouge, math_engine: nil)
-      html = markdown.to_html
-
-      # Undo kramdown's parsing of $$math$$ -- mathjax will handle it
-      html.gsub!(%r{<span class="kdmath">(.*?)</span>}, '$\1$')
-
-      autolink(html)
-    end
-
-    # Render with github-markup
-    # Known problems:
-    #   * code blocks / syntax highlighting not working (TODO)
-    def render_markdown_github(text)
-      autolink(GitHub::Markup.render('filename.md', text))
-    end
-
-    # Render with html-pipeline
-    def render_markdown_pipeline(text)
-      # Syntax pipeline uses css class "highlight-#{lang}" by default.
-      # This overrides that to just use "highlight".
-      context = { scope: 'highlight' }
-      filters = [
-        HTML::Pipeline::MarkdownFilter,
-        HTML::Pipeline::SyntaxHighlightFilter,
-        HTML::Pipeline::AutolinkFilter
-      ]
-      pipeline = HTML::Pipeline.new(filters, context)
-      pipeline.call(text)[:output].to_s
-    end
-
-    # For testing/comparing
-    def render_markdown_all(text)
-      html = '<hr><b><i>redcarpet</i></b><br>'
-      html += render_markdown_redcarpet(text) + '<br>'
-      html += '<hr><b><i>kramdown</i></b><br>'
-      html += render_markdown_kramdown(text) + '<br>'
-      html += '<hr><b><i>github</i></b><br>'
-      html += render_markdown_github(text) + '<br>'
-      html += '<hr><b><i>pipeline</i></b><br>'
-      html += render_markdown_pipeline(text) + '<br>'
-      html + '<hr>'
-    end
-
-    def render_markdown(text, method=:pipeline)
-      func = "render_markdown_#{method}".to_sym
-      html = send(func, text)
-      ActionController::Base.helpers.sanitize(html)
-    end
-
-    def tilt_render(_, _, text)
-      output_protector.unprotect(interpolation.call(render_markdown(text)))
-    end
-  end
-
-  Slim::Embedded.register :markdown, Renderer
-
   def markdown(text)
-    @renderer ||= Renderer.new
-    @renderer.render_markdown text
+    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+    @renderer.render(text)
   end
 
   def code(lang, text)
@@ -107,17 +12,14 @@ module NotebooksHelper
       user_pref = nil
     end
     lang = 'text' if lang.blank?
-    if user_pref && user_pref.disable_row_numbers
-      markdown "```#{lang}\n#{text}\n```"
-    else
-      formatter = Rouge::Formatters::HTML.new
-      begin
-        output=Rouge.highlight text, lang, Rouge::Formatters::HTMLLinewise.new(formatter, class: "code-block")
-      rescue StandardError
-        output=Rouge.highlight text, 'text', Rouge::Formatters::HTMLLinewise.new(formatter, class: "code-block")
-      end
-      output = "<pre class=\"Highlight\">" + output + "</pre>"
+    formatter = Rouge::Formatters::HTML.new
+    formatter = Rouge::Formatters::HTMLLinewise.new(formatter, class: "code-block") unless user_pref && user_pref.disable_row_numbers
+    begin
+      output = Rouge.highlight text, lang, formatter
+    rescue StandardError
+      output = Rouge.highlight text, 'text', formatter
     end
+    "<pre class=\"Highlight\">" + output + "</pre>"
   end
 
   def raw(text)
