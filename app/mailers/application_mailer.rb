215eca658546aeb21c892ca46446b5d938506614
manfromjupyter <51969207+manfromjupyter@users.noreply.github.com>
2023-06-14 16:54:28 +0000

    Setup custom email headers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: GalleryConfig.email.general_from
   layout 'mailer'
+  after_action :add_custom_headers
+
 
   def need_to_simplify_email?(object, message="")
     if GalleryConfig.email.force_simplified_emails
@@ -11,6 +13,15 @@ class ApplicationMailer < ActionMailer::Base
       return object.simplify_email?(message)
     else
       return false
+    end
+  end
+
+  private
+  def add_custom_headers
+    if GalleryConfig.email.email_headers.enabled
+      for header in GalleryConfig.email.email_headers.headers
+        headers[header.key] = header.value
+      end
     end
   end
 
