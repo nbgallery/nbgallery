b52b9d66bc277d63a1c85a03d9ee7d7e666726f6
Hosam Aly <hosamaly6@gmail.com>
2023-08-18 12:48:00 +0000

    Fix test errors (#890)
    * [dev] Avoid loading development gems in test mode
    
    Gems like `web-console` complain about running in test mode, preventing
    tests from running.
    
    * [dev] Fix RuboCop configuration
    
    * [tests] Fix test fixtures
    
    1. Delete the unused files for "notebook_storages"
    
    They were introduced in commit 5687e703d494e74642837b01d3b5de8511c049b9,
    apparently as an original name for what was later called
    "notebook_files".
    
    They haven't been used since, and the fixure causes an error when trying
    to load tests.
    
    2. Delete non-existing fields from preferences fixture
    
    The `url` and `service` fields were deleted in the migration called
    '20160916200000_update_preferences.rb', which was introduced in commit
    a8baf5cd81dca927f7280049458e817c19a35d18.
    
    3. Fix incorrectly-named field in site_warnings fixture
    
    The field `type` was renamed to `level` in the migration
    '20160609222659_rename_warning_type_to_level.rb', which was introduced
    in commit a8baf5cd81dca927f7280049458e817c19a35d18.
    
    4. Fix incorrectly named field in deprecated_notebooks fixture
    
    The field `freeze` has been named `disable_usage` since its introduction
    in commit 0759c994b28f6da76371f8a696dedd4b15fffd84.
    
    5. Fix fixtures data to match schema requirements
    
    * [test] Fix errors in controller tests
    
    Many errors were caused by non-existing routes. Others needed Devise
    ControllerHelpers. And user fixtures needed user_summaries.
    
    * [test] Fix errors in mailer tests

--- test/controllers/stages_controller_test.rb
+++ test/controllers/stages_controller_test.rb
@@ -12,11 +12,6 @@ class StagesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:stages)
   end
 
-  test 'should get new' do
-    get :new
-    assert_response :success
-  end
-
   test 'should create stage' do
     assert_difference('Stage.count') do
       post :create, params: { stage: { user_id: @stage.user_id, uuid: @stage.uuid } }
@@ -28,16 +23,6 @@ class StagesControllerTest < ActionController::TestCase
   test 'should show stage' do
     get :show, params: { id: @stage }
     assert_response :success
-  end
-
-  test 'should get edit' do
-    get :edit, params: { id: @stage }
-    assert_response :success
-  end
-
-  test 'should update stage' do
-    patch :update, params: { id: @stage, stage: { user_id: @stage.user_id, uuid: @stage.uuid } }
-    assert_redirected_to stage_path(assigns(:stage))
   end
 
   test 'should destroy stage' do
