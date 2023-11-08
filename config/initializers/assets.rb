a1354d2811643f9ab3acf13ca7684dca85c4f650
mcrutch <51446717+mcrutch@users.noreply.github.com>
2023-11-08 15:40:04 +0000

    removing unneeded code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,10 +11,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-#Rails.application.config.assets.precompile += %w[
-#  commontator/upvote.png
-#  commontator/upvote_active.png
-#  commontator/upvote_disabled.png
-#  commontator/upvote_hover.png
-#  tooltips.js
-#]
