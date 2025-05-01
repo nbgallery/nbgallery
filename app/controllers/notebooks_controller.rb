f5b27b87b507d256b613c02c2fd81dcd444fe9ed
Universe-sudojam <173473046+Universe-sudojam@users.noreply.github.com>
2025-05-01 21:53:20 +0000

    WIP: remove package field off of filter terms

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -966,7 +966,7 @@ class NotebooksController < ApplicationController
     query = params[:query]
     words = params[:words] if params[:words].present?
     results = {}
-    results[:filter_term] = ["owner","creator","updater","description","tags","lang","title","user","package"]
+    results[:filter_term] = ["owner","creator","updater","description","tags","lang","title","user"]
     if Rails.application.config.searchable_fields.key?(query)
       # Execute the query for the specific field
       lambda = Rails.application.config.searchable_fields[query]
