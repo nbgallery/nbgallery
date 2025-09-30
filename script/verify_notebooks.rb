bc3ea666fa7e3ab0563fd158ab6b3caf836d9e36
eldenlord204824 <witowns@uwe.nsa.gov>
2025-09-30 12:05:16 -0400

    fix nb verify script

--- script/verify_notebooks.rb
+++ script/verify_notebooks.rb
@@ -2,9 +2,16 @@
 
 puts "Verifying notebooks not properly verified..."
 
-Notebook.find_each do |nb|
+notebooks = Notebook.all
+notebook_total = notebooks.length
+notebook_count = 0
+
+notebooks.each do |nb|
+  print "Setting Verification for #{nb.title}..."
   nb.set_verification(nb.review_status == :full)
   nb.save! if nb.changed?
+  notebook_count = notebook_count + 1
+  puts "Done (#{notebook_count}/#{notebook_total})"
 end
 
 puts "Complete"
